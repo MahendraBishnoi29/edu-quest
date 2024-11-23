984c3738ab3163930fb7e3707febc98cbabe279b
Mahendra Bishnoi <mahendrabrowny@gmail.com>
2024-11-23 10:45:23 +0530

    base frontend setup

---
+++ client/src/state/index.ts
@@ -0,0 +1,31 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+interface InitialStateTypes {
+  courseEditor: {
+    sections: Section[];
+    isChapterModalOpen: boolean;
+    isSectionModalOpen: boolean;
+    selectedSectionIndex: number | null;
+    selectedChapterIndex: number | null;
+  };
+}
+
+const initialState: InitialStateTypes = {
+  courseEditor: {
+    sections: [],
+    isChapterModalOpen: false,
+    isSectionModalOpen: false,
+    selectedSectionIndex: null,
+    selectedChapterIndex: null,
+  },
+};
+
+export const globalSlice = createSlice({
+  name: "global",
+  initialState,
+  reducers: {},
+});
+
+export const {} = globalSlice.actions;
+
+export default globalSlice.reducer;
