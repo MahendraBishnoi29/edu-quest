f35c594c9ce0ce1396dd50e94b964de1b6f476d9
Mahendra Bishnoi <mahendrabrowny@gmail.com>
2024-11-26 10:47:51 +0530

    feat[server]: CRUD for courses

--- server/src/controllers/courseController.ts
+++ server/src/controllers/courseController.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from "express";
 import Course from "../models/courseModel";
+import { v4 as uuidv4 } from "uuid";
+import { getAuth } from "@clerk/express";
 
 export const listCourses = async (
   req: Request,
@@ -34,5 +36,130 @@ export const getCourse = async (req: Request, res: Response): Promise<void> => {
       .json({ message: "course retrieved successfully", data: course });
   } catch (error) {
     res.status(500).json({ message: "error retrieving course", error });
+  }
+};
+
+export const createCourse = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const { teacherId, teacherName } = req.body;
+    if (!teacherId || !teacherName) {
+      res.status(404).json({ message: "Teacher Id and name are required" });
+      return;
+    }
+
+    const newCourse = new Course({
+      courseId: uuidv4(),
+      teacherId,
+      teacherName,
+      title: "Untitled Course",
+      description: "",
+      category: "Uncategorized",
+      image: "",
+      price: 0,
+      level: "Beginner",
+      status: "Draft",
+      sections: [],
+      enrollments: [],
+    });
+
+    await Course.save();
+
+    res
+      .status(200)
+      .json({ message: "Course created successfully", data: newCourse });
+  } catch (error) {
+    res.status(500).json({ message: "error creating course", error });
+  }
+};
+
+export const updateCourse = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  const { courseId } = req.params;
+  const updateData = { ...req.body };
+  const { userId } = getAuth(req);
+
+  try {
+    const course = await Course.get(courseId);
+
+    if (!course) {
+      res.status(404).json({ message: "course not found" });
+      return;
+    }
+
+    if (course.teacherId !== userId) {
+      res
+        .status(403)
+        .json({ message: "You are not authorized to update this course" });
+      return;
+    }
+
+    if (updateData.price) {
+      const price = parseFloat(updateData.price);
+      if (isNaN(price)) {
+        res.status(400).json({ message: "Price must be a valid number" });
+        return;
+      }
+      updateData.price = price * 100;
+    }
+
+    if (updateData.sections) {
+      const sectionsData =
+        typeof updateData.sections === "string"
+          ? JSON.parse(updateData.sections)
+          : updateData.sections;
+
+      updateData.sections = sectionsData.map((section: any) => ({
+        ...section,
+        sectionId: section.sectionId || uuidv4(),
+        chapters: section.chapters.map((chapter: any) => ({
+          ...chapter,
+          chapterId: chapter.chapterId || uuidv4(),
+        })),
+      }));
+    }
+
+    Object.assign(course, updateData);
+    await course.save();
+
+    res
+      .status(200)
+      .json({ message: "Course updated successfully", data: course });
+  } catch (error) {
+    res.status(500).json({ message: "error updating course", error });
+  }
+};
+
+export const deleteCourse = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  const { courseId } = req.params;
+  const { userId } = getAuth(req);
+
+  try {
+    const course = await Course.get(courseId);
+
+    if (!course) {
+      res.status(404).json({ message: "course not found" });
+      return;
+    }
+
+    if (course.teacherId !== userId) {
+      res
+        .status(403)
+        .json({ message: "You are not authorized to delete this course" });
+      return;
+    }
+
+    await Course.delete(courseId);
+
+    res.status(200).json({ message: "Course deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "error deleting course", error });
   }
 };
--- server/src/routes/courseRoute.ts
+++ server/src/routes/courseRoute.ts
@@ -1,9 +1,21 @@
+import { requireAuth } from "@clerk/express";
 import express from "express";
-import { getCourse, listCourses } from "../controllers/courseController";
+import multer from "multer";
+import {
+  createCourse,
+  deleteCourse,
+  getCourse,
+  listCourses,
+  updateCourse,
+} from "../controllers/courseController";
 
 const router = express.Router();
+const upload = multer({ storage: multer.memoryStorage() });
 
 router.get("/", listCourses);
+router.post("/", requireAuth(), createCourse);
 router.get("/:courseId", getCourse);
+router.put("/:courseId", requireAuth(), upload.single("image"), updateCourse);
+router.delete("/:courseId", requireAuth(), deleteCourse);
 
 export default router;
