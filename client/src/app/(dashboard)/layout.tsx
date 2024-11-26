9055ae893c172fe06f72cebf870740154575c22c
Mahendra Bishnoi <mahendrabrowny@gmail.com>
2024-11-26 08:40:36 +0530

    feat[client]: stripe payments for purchasing courses

--- client/src/app/(dashboard)/layout.tsx
+++ client/src/app/(dashboard)/layout.tsx
@@ -8,7 +8,6 @@ import { cn } from "@/lib/utils";
 import { useUser } from "@clerk/nextjs";
 import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
-import ChaptersSidebar from "./user/courses/[courseId]/chapter-sidebar";
 
 export default function DashboardLayout({
   children,
@@ -39,7 +38,7 @@ export default function DashboardLayout({
       <div className="dashboard">
         <AppSidebar />
         <div className="dashboard__content">
-          {courseId && <ChaptersSidebar />}
+          {/* {courseId && <ChaptersSidebar />} */}
           <div
             className={cn(
               "dashboard__main",
--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { BaseQueryApi, FetchArgs } from "@reduxjs/toolkit/query";
@@ -8,7 +9,6 @@ import { toast } from "sonner";
 const customBaseQuery = async (
   args: string | FetchArgs,
   api: BaseQueryApi,
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   extraOptions: any
 ) => {
   const baseQuery = fetchBaseQuery({
@@ -23,13 +23,15 @@ const customBaseQuery = async (
   });
 
   try {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const result: any = await baseQuery(args, api, extraOptions);
 
     if (result.error) {
       const errorData = result.error.data;
-      const errorMessage = errorData?.message || "An error occurred";
-      toast.error("Error " + errorMessage);
+      const errorMessage =
+        errorData?.message ||
+        result.error.status.toString() ||
+        "An error occurred";
+      toast.error(`Error: ${errorMessage}`);
     }
 
     const isMutationRequest =
@@ -37,26 +39,24 @@ const customBaseQuery = async (
 
     if (isMutationRequest) {
       const successMessage = result.data?.message;
-      if (successMessage) {
-        toast.success(successMessage);
-      }
+      if (successMessage) toast.success(successMessage);
     }
 
     if (result.data) {
       result.data = result.data.data;
     } else if (
       result.error?.status === 204 ||
-      result.meta?.response?.status === 204
+      result.meta?.response?.status === 24
     ) {
       return { data: null };
     }
 
     return result;
-  } catch (error) {
+  } catch (error: unknown) {
     const errorMessage =
-      error instanceof Error ? error.message : "An error occurred";
+      error instanceof Error ? error.message : "Unknown error";
 
-    return { error: { status: "FETCH_ERROR", message: errorMessage } };
+    return { error: { status: "FETCH_ERROR", error: errorMessage } };
   }
 };
 
@@ -88,8 +88,32 @@ export const api = createApi({
       query: (id) => `courses/${id}`,
       providesTags: (result, error, id) => [{ type: "Courses", id }],
     }),
+
+    createStripePaymentIntent: build.mutation<
+      { clientSecret: string },
+      { amount: number }
+    >({
+      query: ({ amount }) => ({
+        url: `/transactions/stripe/payment-intent`,
+        method: "POST",
+        body: { amount },
+      }),
+    }),
+
+    createTransaction: build.mutation<Transaction, Partial<Transaction>>({
+      query: (transaction) => ({
+        url: "transactions",
+        method: "POST",
+        body: transaction,
+      }),
+    }),
   }),
 });
 
-export const { useGetCoursesQuery, useGetCourseQuery, useUpdateUserMutation } =
-  api;
+export const {
+  useGetCoursesQuery,
+  useGetCourseQuery,
+  useUpdateUserMutation,
+  useCreateStripePaymentIntentMutation,
+  useCreateTransactionMutation,
+} = api;
